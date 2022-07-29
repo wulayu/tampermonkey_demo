9f1bab0e9c6dca7fd6161347723f3a5ae6398ccb
soong <849519573@qq.com>
2022-07-29 16:38:21 +0800

    main

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         收集填入重量
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      test
 // @description  访问http://192.168.201.198:4000/stock_items/check_out_by_wave?q[stock_house_id_eq]=56，获取订单编号
 // @author       Soong
 // @match        http://192.168.201.198:4000/stock_items/check_out_by_wave?q[stock_house_id_eq]=56
 // @match        https://ezeeship.com/newstyle/*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=es.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=water.com
 // @grant        GM_setClipboard
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -49,30 +49,35 @@ if (webSite == "http://192.168.201.198:4000/stock_items/check_out_by_wave?q[stoc
     $(document).ready(function () {
         console.log("*** 填入重量 ***");
 
-        setTimeout(function () {  // 延时等待vue渲染完成
-            console.info($('.el-card__header'))
-            $('.el-card__header').before(
-                '<button type="button" id="showwwww">填入重量</button>');
+        var request_interval = setInterval(checkIsExist, 5000);
+        // 延时等待vue渲染完成
+        function checkIsExist() {
+            if ($('.el-card__header')) {
+                $('.el-card__header').before(
+                    '<button type="button" id="showwwww">填入重量</button>');
 
-            $("#showwwww").on("click", function () {
-                var output = GM_getValue("save");
-                console.log("tampermonkey_output:" + output);
+                $("#showwwww").on("click", function () {
+                    var output = GM_getValue("save");
+                    console.log("tampermonkey_output:" + output);
 
-                output = JSON.parse(output);
-                console.log(output.weight);
+                    output = JSON.parse(output);
+                    console.log(output.weight);
 
-                console.log("*** pasted ***");
-                console.log($("#app").get());
+                    console.log("*** pasted ***");
+                    console.log($("#app").get());
 
-                // 可以用$("#app").__vue__命令在chrome console里查看
-                // $("#app")[0].__vue__.$children[1].$children[3].$refs.numberInfo.order.orderNo = "order#";
-                
-                $("#app")[0].__vue__.$children[1].$children[3].$refs.packageInfo.packages[0].weight = output.weight;
+                    // 可以用$("#app").__vue__命令在chrome console里查看
+                    // $("#app")[0].__vue__.$children[1].$children[3].$refs.numberInfo.order.orderNo = "order#";
 
-                // $("#app")[0].__vue__.$children[1].$children[3].$refs.recipientAddress.$vnode.componentOptions.propsData.orderInfo.recipientAddrId = 130804;
-            });
-        }, 10000)
+                    $("#app")[0].__vue__.$children[1].$children[3].$refs.packageInfo.packages[0].weight = output.weight;
 
+                    // $("#app")[0].__vue__.$children[1].$children[3].$refs.recipientAddress.$vnode.componentOptions.propsData.orderInfo.recipientAddrId = 130804;
+
+                });
+                window.clearInterval(request_interval);
+            }
+
+        }
     });
-
+    
 }
